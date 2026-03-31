a49956744dd0432917a0db7e079a0d2a12d3a365
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-03-31 16:47:41 +0200

    Create blogs.js

---
+++ part4/bloglist/4.12/blogs.js
@@ -0,0 +1,25 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogRouter.post('/', async (request, response) => {
+  if(!request.body.title||!request.body.url)
+    response.status(400).end()
+  else{
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes??0,
+    })
+
+    const result=await blog.save()
+    response.status(201).json(result)
+  }
+})
+
+module.exports = blogRouter
