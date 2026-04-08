97dc75b64f5c96454060f941146691b9d1449943
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-04-08 12:06:48 +0200

    Create blogs.js

---
+++ part4/bloglist/4.20/blogs.js
@@ -0,0 +1,64 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', {username:1, name:1})
+  response.json(blogs)
+})
+
+blogRouter.post('/', async (request, response) => {
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing' })
+  }
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  if (!request.body.title || !request.body.url)
+    response.status(400).end()
+  else {
+    const user = await User.findById(decodedToken.id)
+
+    if (!user) {
+      return response.status(400).json({ error: 'userId missing or not valid' })
+    }
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes ?? 0,
+      user: user._id
+    })
+
+    const result = await blog.save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+    response.status(201).json(result)
+  }
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
+  }
+
+  blog.title = body.title ?? blog.title
+  blog.author = body.author ?? blog.author
+  blog.url = body.url ?? blog.url
+  blog.likes = body.likes ?? blog.likes
+
+  const result = await blog.save()
+  response.json(result)
+})
+
+module.exports = blogRouter
