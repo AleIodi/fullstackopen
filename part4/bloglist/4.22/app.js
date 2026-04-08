64b17f80e9fcf7bcd3b308fe366a8de674567875
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-04-08 12:38:05 +0200

    Create app.js

---
+++ part4/bloglist/4.22/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+.then(() => console.log('Connesso a MongoDB'))
+.catch(err => console.error('Errore di connessione:', err))
+
+app.use(express.json())
+morgan.token('body', (req, res) => {
+  const body = { ...req.body }
+  
+  if (body.password) {
+    body.password = '***'
+  }
+  
+  return JSON.stringify(body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(express.static('dist'))
+
+app.use(middleware.tokenExtractor)
+
+app.use('/api/blogs', middleware.userExtractor, blogRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
