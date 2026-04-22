174fde67c7fce61a89217d5ecfa3bc2013a1c805
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-04-22 14:53:35 +0200

    Create store.js

---
+++ part6/6.11/store.js
@@ -0,0 +1,76 @@
+
+import { create } from 'zustand'
+import anecdoteService from './services/anecdotes'
+
+let timeoutId = null
+
+const useNotificationStore = create((set) => ({
+  message: '',
+  show: false,
+
+  showNotification: (message, time = 5) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+
+    set({ message, show: true })
+
+    timeoutId = setTimeout(() => {
+      set({ message: '', show: false })
+    }, time * 1000)
+  }
+}))
+
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const useAnecdoteStore = create((set, get) => ({
+  anecdotes: [],
+  filter: '',
+  actions: {
+    addVote: async (id) => {
+      const anec = get().anecdotes.find(a => a.id === id)
+      const response = await anecdoteService.update(id, { ...anec, votes: anec.votes + 1 })
+      set(state => ({ anecdotes: state.anecdotes.map(a => a.id === id ? response : a) }))
+      useNotificationStore.getState().showNotification(`you voted '${anec.content}'`)
+    },
+
+    addAnectode: async (anectode) => {
+      const newAnecdote = await anecdoteService.createNew(anectode)
+      set(state => ({ anecdotes: state.anecdotes.concat(newAnecdote) }))
+      useNotificationStore.getState().showNotification(`you added '${newAnecdote.content}'`)
+    },
+
+    setFilter: value => set({ filter: value }),
+
+    inizialize: async () => {
+      const arr = await anecdoteService.getAll()
+      set({ anecdotes: arr })
+    },
+
+    removeAnecdote: async (id) => {
+      const anec = get().anecdotes.find(a => a.id === id)
+      if (window.confirm(`Delete the anecdote '${anec.content}'?`)) {
+        const response = await anecdoteService.remove(id)
+        if (response) {
+          set(state => ({ anecdotes: state.anecdotes.filter(a => a.id !== id) }))
+          useNotificationStore.getState().showNotification(`Anecdote '${anec.content}' deleted`)
+        }
+      }
+    }
+  },
+}))
+
+export const useNotificationMessage = () => useNotificationStore(s => s.message)
+export const useNotificationShow = () => useNotificationStore(s => s.show)
+
+export const useAnecdotes = () => {
+  const anecdotes = useAnecdoteStore((state) => state.anecdotes)
+  const filter = useAnecdoteStore((state) => state.filter)
+
+  if (filter.trim() === '')
+    return anecdotes
+  return anecdotes.filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+}
+export const useFilter = () => useAnecdoteStore((state) => state.filter)
+export const useAnecdoteActions = () => useAnecdoteStore((state) => state.actions)
