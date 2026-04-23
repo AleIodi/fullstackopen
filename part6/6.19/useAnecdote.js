bfb64a98ad52bec60e05a3031a9f64a88b4f8763
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-04-23 16:38:25 +0200

    Create useAnecdote.js

---
+++ part6/6.19/useAnecdote.js
@@ -0,0 +1,44 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAll, createNew, update } from '../requests'
+
+export const useAnecdotes = () => {
+	const queryClient = useQueryClient()
+
+	const updateAnecdoteMutation = useMutation({
+		mutationFn: update,
+		onSuccess: (updatedAnecdote) => {
+			const anecdotes = queryClient.getQueryData(['anecdotes'])
+			queryClient.setQueryData(
+				['anecdotes'],
+				anecdotes.map(a => a.id === updatedAnecdote.id ? updatedAnecdote : a)
+			)
+		}
+	})
+
+	const newAnecdoteMutation = useMutation({
+		mutationFn: createNew,
+		onSuccess: (anecdote) => {
+			const anecdotes = queryClient.getQueryData(['anecdotes'])
+			queryClient.setQueryData(['anecdotes'], anecdotes.concat(anecdote))
+		},
+		onError: (error) => {
+			alert(error.message)
+		}
+	})
+
+	const result = useQuery({
+		queryKey: ['anecdotes'],
+		queryFn: getAll,
+		refetchOnWindowFocus: false
+	})
+
+	const anecdotes = result.data
+
+	return {
+		isPending: result.isPending,
+		isError: result.isError,
+		anecdotes,
+		vote: (anecdote) => updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes+1}),
+		addAnecdote: (content) => newAnecdoteMutation.mutate({ id: (100000 * Math.random()).toFixed(0), content, votes: 0 })
+	}
+}
