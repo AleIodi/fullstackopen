1a5d178539640651ad57b1239c2bd667c7261ffe
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-04-24 15:21:43 +0200

    Create CreateNew.jsx

---
+++ part7/7.2/CreateNew.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useField } from '../hooks'
+
+const CreateNew = ({ addAnecdote }) => {
+  const navigate = useNavigate()
+  const { reset: resetContent, ...content } = useField('text')
+  const { reset: resetAuthor, ...author } = useField('text')
+  const { reset: resetInfo, ...info } = useField('text')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addAnecdote({ content: content.value, author: author.value, info: info.value, votes: 0 })
+    navigate('/')
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input name='content' {...content} />
+        </div>
+        <div>
+          author
+          <input name='author' {...author} />
+        </div>
+        <div>
+          url for more info
+          <input name='info' {...info} />
+        </div>
+        <button>create</button>
+        <button type='button' onClick={() => {
+          resetAuthor()
+          resetContent()
+          resetInfo()
+        }}>reset</button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
