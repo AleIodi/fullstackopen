74fe22d9309d195dd1f8c4e462bb464971adf4e9
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-05-06 15:10:30 +0200

    Update store.js

--- part7/zustand/7.12/store.js
+++ part7/zustand/7.12/store.js
@@ -1,63 +1,78 @@
-import { create } from 'zustand'
-import blogService from './src/services/blogs'
+import { create } from "zustand";
+import blogService from "./src/services/blogs";
 
 const useNotificationStore = create((set, get) => ({
-	message: "",
-	show: false,
-	timeoutId: null,
-	type: "success" | "error",
-	showNotification: (message, type = "success", duration = 5) => {
-		const currentTimeout = get().timeoutId
-		if (currentTimeout) {
-			clearTimeout(currentTimeout)
-		}
+  message: "",
+  show: false,
+  timeoutId: null,
+  type: "success" | "error",
+  showNotification: (message, type = "success", duration = 5) => {
+    const currentTimeout = get().timeoutId;
+    if (currentTimeout) {
+      clearTimeout(currentTimeout);
+    }
 
-		set({ message, show: true, type })
+    set({ message, show: true, type });
 
-		const id = setTimeout(() => {
-			set({ message: "", show: false, timeoutId: null })
-		}, duration * 1000);
+    const id = setTimeout(() => {
+      set({ message: "", show: false, timeoutId: null });
+    }, duration * 1000);
 
-		set({ timeoutId: id })
-	}
-}))
+    set({ timeoutId: id });
+  },
+}));
 
-export const useNotify = () => useNotificationStore(state => state.showNotification)
-export const useNotifyMessage = () => useNotificationStore(state => state.message)
-export const useNotifyIsVisible = () => useNotificationStore(state => state.show)
-export const useNotifyType = () => useNotificationStore(state => state.type)
+export const useNotify = () =>
+  useNotificationStore((state) => state.showNotification);
+export const useNotifyMessage = () =>
+  useNotificationStore((state) => state.message);
+export const useNotifyIsVisible = () =>
+  useNotificationStore((state) => state.show);
+export const useNotifyType = () => useNotificationStore((state) => state.type);
 
 const useBlogStore = create((set, get) => ({
-	blogs: [],
-	isLoading: false,
-	actions: {
-		inizialize: async () => {
-			set({ isLoading: true });
-			try {
-				const blogs = await blogService.getAll()
-				set({ blogs })
-			} catch {
-				useNotificationStore.getState().showNotification("Failed to fetch blogs", "error");
-			} finally {
-				set({ isLoading: false });
-			}
-		},
+  blogs: [],
+  isLoading: false,
+  actions: {
+    inizialize: async () => {
+      set({ isLoading: true });
+      try {
+        const blogs = await blogService.getAll();
+        set({ blogs });
+      } catch {
+        useNotificationStore
+          .getState()
+          .showNotification("Failed to fetch blogs", "error");
+      } finally {
+        set({ isLoading: false });
+      }
+    },
 
-		addBlog: async (newBlog) => {
-			try {
-				const blog = await blogService.create(newBlog)
-				set(state => ({ blogs: state.blogs.concat(blog) }))
-				useNotificationStore.getState().showNotification(`a new blog '${blog.title}' by '${blog.author}' added`)
-			} catch (exception) {
-				const serverErrorMessage =
-					exception.response?.data?.error ?? "Failed to create new blog";
+    addBlog: async (newBlog) => {
+      try {
+        const blog = await blogService.create(newBlog);
+        set((state) => ({ blogs: state.blogs.concat(blog) }));
+        useNotificationStore
+          .getState()
+          .showNotification(
+            `a new blog '${blog.title}' by '${blog.author}' added`,
+          );
 
-				useNotificationStore.getState().showNotification(serverErrorMessage, "error");
-			}
-		},
-	}
-}))
+        return true;
+      } catch (exception) {
+        const serverErrorMessage =
+          exception.response?.data?.error ?? "Failed to create new blog";
 
-export const useBlogIsLoading = () => useBlogStore((state) => state.isLoading)
-export const useBlogs = () => useBlogStore((state) => state.blogs)
-export const useBlogActions = () => useBlogStore((state) => state.actions)
+        useNotificationStore
+          .getState()
+          .showNotification(serverErrorMessage, "error");
+
+        return false;
+      }
+    },
+  },
+}));
+
+export const useBlogIsLoading = () => useBlogStore((state) => state.isLoading);
+export const useBlogs = () => useBlogStore((state) => state.blogs);
+export const useBlogActions = () => useBlogStore((state) => state.actions);
