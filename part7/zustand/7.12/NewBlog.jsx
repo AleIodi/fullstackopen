2070a0058f7bcc1cff1db2078ecbe22a07ffd6d2
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-05-06 14:26:09 +0200

    Create NewBlog.jsx

---
+++ part7/zustand/7.12/NewBlog.jsx
@@ -0,0 +1,73 @@
+import { Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useBlogActions } from "../../store";
+
+const NewBlog = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+  const navigate = useNavigate();
+  const { addBlog } = useBlogActions()
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addBlog({ title, author, url });
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h2>Create New</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <TextField
+            sx={{ width: 500 }}
+            required
+            label="title"
+            variant="outlined"
+            value={title}
+            size="small"
+            margin="dense"
+            onChange={({ target }) => setTitle(target.value)}
+            name="Title"
+          />
+        </div>
+        <div>
+          <TextField
+            sx={{ width: 500 }}
+            required
+            label="author"
+            variant="outlined"
+            value={author}
+            size="small"
+            margin="dense"
+            onChange={({ target }) => setAuthor(target.value)}
+            name="Author"
+          />
+        </div>
+        <div>
+          <TextField
+            sx={{ width: 500 }}
+            required
+            label="url"
+            variant="outlined"
+            value={url}
+            size="small"
+            margin="dense"
+            onChange={({ target }) => setUrl(target.value)}
+            name="Url"
+          />
+        </div>
+        <Button sx={{ marginTop: "1em" }} variant="contained" type="submit">
+          CREATE
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewBlog;
