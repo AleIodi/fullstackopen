3a450fdecaec29bff68ad5198b914d937e68e261
Alessandro Iodice <alessandro.iodice@itsrizzoli.it>
2026-05-06 12:16:02 +0200

    Create store.js

---
+++ part7/zustand/7.11/store.js
@@ -0,0 +1,27 @@
+import { create } from 'zustand'
+
+const useNotificationStore = create((set, get) => ({
+	message: "",
+	show: false,
+	timeoutId: null,
+	type: "success" | "error",
+	showNotification: (message, type = "success", duration = 5) => {
+		const currentTimeout = get().timeoutId
+		if (currentTimeout) {
+			clearTimeout(currentTimeout)
+		}
+
+		set({ message, show: true, type })
+
+		const id = setTimeout(() => {
+			set({ message: "", show: false, timeoutId: null })
+		}, duration * 1000);
+
+		set({ timeoutId: id })
+	}
+}))
+
+export const useNotify = () => useNotificationStore(state => state.showNotification)
+export const useNotifyMessage = () => useNotificationStore(state => state.message)
+export const useNotifyIsVisible = () => useNotificationStore(state => state.show)
+export const useNotifyType = () => useNotificationStore(state => state.type)
